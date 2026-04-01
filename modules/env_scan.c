9f79af6481631219d222f98262379100bd15b987
Aaryan-khairnar(arch) <aaryankhairnar.amk@gmail.com>
2026-04-01 15:31:24 +0530

    Completed env scan v1.0 Next steps: - Split the whole thing in Key and Value - Store in different lists

--- modules/env_scan.c
+++ modules/env_scan.c
@@ -3,48 +3,68 @@
 This module covers Environment Variables and Stores it in a key value pair
 */
 
-/*
-environ
-   ↓                                      char** environ
-+------------------+                        ↓
-| pointer to "A=B" | ---> "A=B\0"        [ char* ] ----> "KEY=VALUE"
-| pointer to "C=D" | ---> "C=D\0"        [ char* ] ----> "USER=arch"
-| pointer to "E=F" | ---> "E=F\0"        [ char* ] ----> "HOME=/home/arch"
-| NULL             |                     [  NULL ]
-+------------------+
-
-env        = environ
-*env       = "KEY=VALUE"
-**env      = 'K'
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include "env_scan.h"
 
+#define MAX_ENV 200
+
 extern char **environ;
 
-void printenv(const char *label, const char *value) {
-  printf("E: %s\t-> V: %s\n", label, value);
+typedef struct {
+  char *entry;
+  int is_critical;
+} EnvVar;
+
+int is_critical_var(char *entry) {
+
+    if (strncmp(entry, "PATH=", 5) == 0) return 2;
+    if (strncmp(entry, "LD_", 3) == 0) return 2;
+    if (strncmp(entry, "USER=", 5) == 0) return 2;
+    if (strncmp(entry, "LOGNAME=", 8) == 0) return 2;
+    if (strncmp(entry, "HOME=", 5) == 0) return 2;
+
+    if (strncmp(entry, "XAUTHORITY=", 11) == 0) return 1;
+    if (strncmp(entry, "DBUS_", 5) == 0) return 1;
+    if (strncmp(entry, "XDG_RUNTIME_DIR=", 16) == 0) return 1;
+    if (strncmp(entry, "DISPLAY=", 8) == 0) return 1;
+
+    return 0;
 }
 
 void run_env_scan(){
 
+  EnvVar list[MAX_ENV];
+  int count = 0;
   char **env = environ;
 
-  while (*env) { 
-    
-    char *entry = *env;
-
-    char *eq = strchr(entry, '=');
-
-    if(eq){
-      *eq = '\0';
-      printenv(entry, eq+1);
-    }
-
+  while (*env && count < MAX_ENV) { 
+    list[count].entry = *env;
+    list[count].is_critical = is_critical_var(*env);
+    count ++;
     env++;
-  }  
+  }
+
+  printf("\n[HIGH importance ENV VARS]\n");
+  for(int i=0; i<count; i++){
+    if (list[i].is_critical == 2){
+      printf("%s\n", list[i].entry);
+    }
+  }
+
+  printf("\n[Interesting ENV VARS]\n");
+  for(int i=0; i<count; i++){
+    if (list[i].is_critical == 1) {
+      printf("%s\n", list[i].entry);
+    }
+  }
+  
+  printf("\n[LOW importance ENV VARS]\n");
+  for(int i=0; i<count; i++){
+    if (list[i].is_critical == 0) {
+      printf("%s\n", list[i].entry);
+    }
+  }
 }
 
 int main(){
@@ -62,3 +82,20 @@ int* x = &k // x stores address of k
 x // address of k
 *x // value of k (dereferenced)
 */
+
+
+/*
+environ
+   ↓                                      char** environ
++------------------+                        ↓
+| pointer to "A=B" | ---> "A=B\0"        [ char* ] ----> "KEY=VALUE"
+| pointer to "C=D" | ---> "C=D\0"        [ char* ] ----> "USER=arch"
+| pointer to "E=F" | ---> "E=F\0"        [ char* ] ----> "HOME=/home/arch"
+| NULL             |                     [  NULL ]
++------------------+
+
+env        = environ
+*env       = "KEY=VALUE"
+**env      = 'K'
+*/
+
